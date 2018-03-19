d33d06530197abc1ba67c7f525078ff9706780eb
KimMJ <ss59s@nate.com>
2018-03-20 01:53:16 +0900

    client struct

--- multiSocket/server.cpp
+++ multiSocket/server.cpp
@@ -10,16 +10,34 @@
 #include <pthread.h>
 
 #define BUFSIZE 100
+#define MAX_CLIENT 10
 
 void* clnt_connection(void *arg);
-void send_message(char *message, int len, int sender_fd);
+void send_message(char *message, int len, struct client clnt);
 void error_handling(char *message);
 
 int clnt_number = 0;
-int clnt_socks[10];
-socklen_t clnt_addr_sizes[10];
+//int clnt_socks[10];
+//socklen_t clnt_addr_sizes[10];
+
+struct client{
+  int socket;
+  socklen_t addr_size;
+
+  client(int socket, socklen_t addr_size){
+    this->socket = socket;
+    this->addr_size = addr_size;
+  }
+
+  client(){
+    this->socket = 0;
+    this->addr_size = 0;
+  }
+};
+struct client clients[MAX_CLIENT];
 
 pthread_mutex_t mutx;
+
 
 int main(int argc, char **argv){
   int serv_sock, clnt_sock;
@@ -61,36 +79,35 @@ int main(int argc, char **argv){
     printf("clnt addr size : %d\n", clnt_addr_size); 
 
     pthread_mutex_lock(&mutx);
-    clnt_addr_sizes[clnt_number] = clnt_addr_size;
-    clnt_socks[clnt_number++] = clnt_sock;
+    struct client tmp(clnt_sock, clnt_addr_size);
+    clients[clnt_number++] = tmp;
     
     pthread_mutex_unlock(&mutx);
 
-    pthread_create(&thread, NULL, clnt_connection, (void *) &clnt_sock);
+    pthread_create(&thread, NULL, clnt_connection, (void *) &tmp);
     printf(" IP : %s \n", inet_ntoa(clnt_addr.sin_addr));
   }
   return 0;
 } 
 
 void *clnt_connection(void *arg){
-  int clnt_sock = *(int *) arg;
+  int clnt_sock = ((struct client *)arg)->socket;
   int str_len = 0;
   char message[BUFSIZE];
   int i;
 
   while((str_len = read(clnt_sock, message, sizeof(message))) != 0){
     struct sockaddr_in peer_addr;
-    getpeername(clnt_sock, (struct sockaddr *) &peer_addr, clnt_addr_sizes + 0); // must change!!
-    send_message(message, str_len, clnt_sock);
+    getpeername(clnt_sock, (struct sockaddr *) &peer_addr, &((struct client *)arg)->addr_size); // must change!!
+    send_message(message, str_len, *(struct client *)arg);
     printf("peer name : %s\n", inet_ntoa(peer_addr.sin_addr));
   }
 
   pthread_mutex_lock(&mutx);
   for (i = 0; i < clnt_number; i ++){
-    if (clnt_sock == clnt_socks[i]){
+    if (clnt_sock == clients[i].socket){
       for (; i < clnt_number - 1; i ++){
-        clnt_socks[i] = clnt_socks[i+1];
-        clnt_addr_sizes[i] = clnt_addr_sizes[i+1];
+        clients[i] = clients[i+1];
       }
       break;
     }
@@ -103,17 +120,17 @@ void *clnt_connection(void *arg){
   return 0;
 }
 
-void send_message(char *message, int len, int sender_fd){
+void send_message(char *message, int len, struct client clnt){
   int i;
   pthread_mutex_lock(&mutx);
   struct sockaddr_in sender_addr, rcv_addr;
-  getpeername(sender_fd, (struct sockaddr *) &sender_addr, clnt_addr_sizes + 0);
+  getpeername(clnt.socket, (struct sockaddr *) &sender_addr, &clnt.addr_size);
   std::string sender = inet_ntoa(sender_addr.sin_addr);
   for (i = 0; i < clnt_number; i ++){
-    getpeername(clnt_socks[i], (struct sockaddr *) &rcv_addr, clnt_addr_sizes + 0);
+    getpeername(clnt.socket, (struct sockaddr *) &rcv_addr, &clnt.addr_size);
     std::string receiver = inet_ntoa(rcv_addr.sin_addr);
     if (sender != receiver){
-      write(clnt_socks[i], message, len);
+      write(clients[i].socket, message, len);
     }
   }
 
